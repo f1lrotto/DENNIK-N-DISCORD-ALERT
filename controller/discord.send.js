f7df031152082c3ce9027eb477396243a7e4ee34
f1lrotto <filip.hupka@gmail.com>
2022-05-12 19:34:11 +0200

    b

--- controller/discord.send.js
+++ controller/discord.send.js
@@ -1,10 +1,15 @@
-const moment = require('moment')
+const moment = require("moment");
 
 function makeDiscordMessage(newArticles) {
-  message = `**There are ${newArticles.length} new articles on Minuta po Minute.**\n\n`;
+  if (newArticles.length == 1) {
+    message = `**There is ${newArticles.length} new article on Minuta po Minute.**\n\n`;
+  } else {
+    message = `**There are ${newArticles.length} new articles on Minuta po Minute.**\n\n`;
+  }
+
   newArticles.forEach((article) => {
-      const time = moment(article.postTime).format("HH:mm");
-      message += `At ${time}, ${article.headline}\n<${article.postLink}>\n\n`;
+    const time = moment(article.postTime).format("HH:mm");
+    message += `At ${time}, ${article.headline}\n<${article.postLink}>\n\n`;
   });
 
   return message;
