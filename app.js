990ae7f79d8b949703f3a2ded4fc1d8ffcbd07ba
f1lrotto <filip.hupka@gmail.com>
2022-06-18 06:29:37 +0200

    changed localhost to hosted url

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 
 const uploadArticles = require("./models/articles.model");
 const { connectMongo } = require("./services/mongo");
-const sendDiscordMessage = require('./services/discord')
+const sendDiscordMessage = require("./services/discord");
 
 require("dotenv").config();
 
@@ -14,10 +14,10 @@ const app = express();
 
 cron.schedule("*/30 * * * *", () => {
   axios
-    .get("http://127.0.0.1:5000/posts")
+    .get("https://dennik-n-scraper.herokuapp.com/posts")
     .then(async (res) => {
-        const newArticles = await uploadArticles(res.data);
-        sendDiscordMessage(newArticles);
+      const newArticles = await uploadArticles(res.data);
+      sendDiscordMessage(newArticles);
     })
     .catch((error) => {
       console.log(error);
